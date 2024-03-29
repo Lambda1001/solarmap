2c69827941dd79c82d4fe9f9de8c72a2a46eac19
Lambda1001 <kvinandk11@gmail.com>
2024-03-29 16:58:56 +0300

    Adding env file

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/solarsystem';
+    protected $redirectTo = 'solarsystem.solarsystem';
 
     /**
      * Create a new controller instance.
