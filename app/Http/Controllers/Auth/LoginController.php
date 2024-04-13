ce2207f9871239a07aacc0e9688d7c7eadd38f65
Lambda1001 <98659925+Lambda1001@users.noreply.github.com>
2024-04-13 13:26:45 +0300

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'solarsystem.solarsystem';
+    protected $redirectTo = 'solarsystem';
 
     /**
      * Create a new controller instance.
